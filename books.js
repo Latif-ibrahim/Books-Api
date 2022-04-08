d6c2c327fa752ec9c12fb8be10e0f5398bc816dd
Latif-Ibrahim <ibrahimabdulatif1@gmail.com>
2022-04-08 07:28:48 -0700

    first commit

---
+++ books.js
@@ -0,0 +1,21 @@
+const books =[
+    {title:"Things fall", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"},
+    {title:"Things fall apart", author:"Chinedu", Publication:"12/03/10",type:"Fiction"}
+]
+
+
+
+
+
+
+module.exports= books
---
+++ controllers/booksController.js
@@ -0,0 +1,28 @@
+const books = require("../books")
+
+
+//get all books
+const getBooks = (req, res)=>{
+    res.status(200).json(books)
+}
+
+
+//delete a book
+/*const deleteBook =async(req, res)=>{
+    const foundBook = await books.findByTitle(res.params.title)
+    if (foundBook){
+        foundBook.remove()
+        res.json({msg:"book removed"})
+    } else{
+        res.status(404).json({error:"Book not found"})
+    }
+}*/
+
+//delete a results
+const deleteBook =(req, res)=>{
+    res.json(books.filter((book)=> book.title!==req.params.title))
+}
+
+
+
+module.exports={getBooks, deleteBook}
